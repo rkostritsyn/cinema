cf28ecf3e8a375a8c50f561cc59e76eb70362298
rkostritsyn <rkostritsyn@griddynamics.com>
2021-04-18 14:56:50 +0300

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Roman Kostritsyn on 18.04.2021.
+//  Copyright © 2021 Roman Kostritsyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
